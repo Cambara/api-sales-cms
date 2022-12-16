5d5cd75b2778ac1a4efdc238e9e64f1134dd4a13
Tiago Cambará <tc@learningbank.io>
2022-12-16 14:51:32 +0000

    feat[sc-71]: create a health check endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { getConfig } from './config/config.loader';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [getConfig],
     }),
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
