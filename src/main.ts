1d09259267b264a217129d57fc8a20ad81eefecf
Tiago Cambará <tc@learningbank.io>
2022-12-16 14:31:44 +0000

    feat[sc-71]: setup the config load

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configEnum } from './config/config.loader';
+import { IHttpConfig } from './config/config_loader.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get<ConfigService>(ConfigService);
+  const { host, port } = configService.get<IHttpConfig>(configEnum.HTTP);
+  await app.listen(port, host);
+  console.log(`Started server on ${host} host and ${port} port`);
 }
 bootstrap();
