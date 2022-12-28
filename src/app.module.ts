9e24725249afb6805f5b35dd75c674cd6f3a7e9b
Tiago Cambará <tc@learningbank.io>
2022-12-28 16:43:15 +0000

    feat[sc-70]: create the user test module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { getConfig } from './config/config.loader';
 import { HealthModule } from './health/health.module';
 import { InfraModule } from './infra/infra.module';
+import { UserTestModule } from './user_test/user_test.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { InfraModule } from './infra/infra.module';
     }),
     InfraModule,
     HealthModule,
+    UserTestModule,
   ],
 })
 export class AppModule {}
