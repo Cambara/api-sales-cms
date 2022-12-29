8fa7c4853355367251ea304e84fd0a1d333316e6
Tiago Cambará <tc@learningbank.io>
2022-12-29 15:12:28 +0000

    feat(sc-72): add root route

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './controllers/health.controller';
+import { RootController } from './controllers/root.controller';
 
 @Module({
-  controllers: [HealthController],
+  controllers: [HealthController, RootController],
 })
 export class HealthModule {}
