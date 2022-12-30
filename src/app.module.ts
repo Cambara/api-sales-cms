d7e786afeb0338f2ffb467ff8715c70110e82923
Tiago Cambará <tc@learningbank.io>
2022-12-30 19:12:53 +0000

    refactor: enable the user test and infra modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { getConfig } from './config/config.loader';
 import { HealthModule } from './health/health.module';
-// import { InfraModule } from './infra/infra.module';
-// import { UserTestModule } from './user_test/user_test.module';
+import { InfraModule } from './infra/infra.module';
+import { UserTestModule } from './user_test/user_test.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [getConfig],
     }),
-    // InfraModule,
+    InfraModule,
     HealthModule,
-    // UserTestModule,
+    UserTestModule,
   ],
 })
 export class AppModule {}
