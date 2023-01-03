da6d05cfdf5589df00b2653b302ca8ff918d9422
Tiago Cambará <tc@learningbank.io>
2023-01-03 19:41:21 +0000

    chore(sc-78): new migrate to create the organization table

---
+++ db/migrations/1672773961332-sc-78-create-organization-table.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class sc78CreateOrganizationTable1672773961332
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'organization',
+        columns: [
+          {
+            name: 'id',
+            type: 'bigint',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+            unsigned: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar(150)',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'datetime',
+            isNullable: false,
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'datetime',
+            isNullable: false,
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('organization');
+  }
+}
