2c4f423962e96efdb12c649fe3456d6e85861aa0
Tiago Cambará <tc@learningbank.io>
2023-01-03 19:53:43 +0000

    chore(sc-78): new migrate to create a job_title table

---
+++ db/migrations/1672775074348-sc-78-create-job_title-table.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class sc78CreateJobTitleTable1672775074348
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'job_title',
+        columns: [
+          {
+            name: 'id',
+            type: 'bigint',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+            unsigned: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar(150)',
+            isNullable: false,
+          },
+          {
+            name: 'is_activated',
+            type: 'bool',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'datetime',
+            isNullable: false,
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'datetime',
+            isNullable: false,
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('job_title');
+  }
+}
