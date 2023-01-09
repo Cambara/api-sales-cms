8d77c208c9bb5f9942666e3140d8be81eca9ceee
Tiago Cambará <tc@learningbank.io>
2023-01-09 20:50:28 +0000

    feat(sc-78): check if has a user with the same email and get the default job title

--- src/credentials/services/credential.service.ts
+++ src/credentials/services/credential.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { EmployeeRepository } from 'src/infra/database/repositories/employee.repository';
+import { JobTitleRepository } from 'src/infra/database/repositories/job_title.repository';
 import { ProfileRepository } from 'src/infra/database/repositories/profile.repository';
 import { UserRepository } from 'src/infra/database/repositories/user.repository';
 import { TransactionHelper } from '../../infra/database/helpers/transaction.helper';
@@ -8,9 +9,11 @@ import { ISignupDto } from '../dtos/signup.dto';
 
 @Injectable()
 export class CredentialService {
+  private readonly jobTitleName = 'ADMINISTRATOR';
   constructor(
     private readonly transactionHelper: TransactionHelper,
     private readonly employeeRepository: EmployeeRepository,
+    private readonly jobTitleRepository: JobTitleRepository,
     private readonly organizationRepository: OrganizationRepository,
     private readonly profileRepository: ProfileRepository,
     private readonly userRepository: UserRepository,
@@ -23,6 +26,20 @@ export class CredentialService {
     firstName,
     lastName,
   }: ISignupDto): Promise<void> {
+    const hasUser = await this.userRepository.findOne({ email });
+
+    if (hasUser) {
+      throw new ConflictException('We already have a user with this e-mail');
+    }
+
+    const jobTitle = await this.jobTitleRepository.findOneByName(
+      this.jobTitleName,
+    );
+
+    if (!jobTitle) {
+      throw new Error('Job title not found');
+    }
+
     await this.transactionHelper.startTransaction();
 
     try {
@@ -43,6 +60,7 @@ export class CredentialService {
         organizationId: organization.id,
         userId: user.id,
         isOwner: true,
+        jobTitleId: jobTitle.id,
       });
       const profilePromise = await this.profileRepository.create({
         firstName,
