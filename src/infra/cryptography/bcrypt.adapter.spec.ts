38a7a29c8d0d7e9358b9ac448ddc6650c4c7cc32
Tiago Cambará <tc@learningbank.io>
2023-01-13 17:49:52 +0000

    test(sc-101): should return a invalid value

--- src/infra/cryptography/bcrypt.adapter.spec.ts
+++ src/infra/cryptography/bcrypt.adapter.spec.ts
@@ -17,6 +17,10 @@ const createSut = () => {
 };
 
 describe('BcryptAdapter', () => {
+  afterEach(async () => {
+    await jest.clearAllMocks();
+  });
+
   describe('encrypt', () => {
     it('Should use the salt class value', async () => {
       const { salt, cryptAdapter } = createSut();
@@ -38,13 +42,24 @@ describe('BcryptAdapter', () => {
   describe('isMatch', () => {
     it('Should match the value with hash', async () => {
       const { cryptAdapter } = createSut();
-      jest.spyOn(bcrypt, 'hash');
+      jest.spyOn(bcrypt, 'compare');
       const value = 'string';
       const isValid = await cryptAdapter.isMatch(value, hashValue);
 
       expect(isValid).toBe(true);
       expect(bcrypt.compare).toBeCalledTimes(1);
       expect(bcrypt.compare).toHaveBeenCalledWith(value, hashValue);
+    });
+
+    it('Should return a invalid value', async () => {
+      const { cryptAdapter } = createSut();
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockImplementationOnce(() => Promise.resolve(false));
+      const value = 'string';
+      const isValid = await cryptAdapter.isMatch(value, hashValue);
+
+      expect(isValid).toBe(false);
     });
   });
 });
