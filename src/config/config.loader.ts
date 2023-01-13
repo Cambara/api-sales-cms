58572cfeb13f51a7953580ba038b5701c37d1f94
Tiago Cambará <tc@learningbank.io>
2023-01-13 18:08:13 +0000

    feat(sc-101): add cryptography env config

--- src/config/config.loader.ts
+++ src/config/config.loader.ts
@@ -2,12 +2,16 @@ import { IConfigLoader } from './config_loader.interface';
 
 export enum configEnum {
   HTTP = 'http',
+  CRYPTOGRAPHY = 'cryptography',
 }
 
 const getConfig = (): IConfigLoader => ({
   http: {
     host: process.env.HTTP_HOST || 'localhost',
     port: process.env.HTTP_PORT ? Number(process.env.HTTP_PORT) : 9000,
+  },
+  cryptography: {
+    salt: Number(process.env.CRYPTOGRAPHY_SALT) || 10,
   },
 });
 
--- src/config/config_loader.interface.ts
+++ src/config/config_loader.interface.ts
@@ -2,6 +2,12 @@ export interface IHttpConfig {
   host: string;
   port: number;
 }
+
+export interface ICryptographyConfig {
+  salt: number;
+}
+
 export interface IConfigLoader {
   http: IHttpConfig;
+  cryptography: ICryptographyConfig;
 }
