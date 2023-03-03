a32ac134c5330de36b29ce4f0af774812dfea18b
Tiago Cambará <tc@learningbank.io>
2023-03-03 16:40:59 +0000

    feat(sc-77): create the temporary token model

---
+++ src/domain/models/temporary_token.ts
@@ -0,0 +1,36 @@
+import {
+  DefaultWithTimestampsModel,
+  IDefaultWithTimestampsModel,
+} from './default.model';
+
+enum TemporaryTokenTypeEnum {
+  FORGOT_PASSWORD = 'forgot_password',
+}
+
+export interface ITemporaryTokenModel extends IDefaultWithTimestampsModel {
+  token: string;
+  type: TemporaryTokenTypeEnum;
+  userIds: number[];
+  expiredAt: Date;
+  usedAt?: Date;
+}
+
+export class TemporaryTokenModel
+  extends DefaultWithTimestampsModel
+  implements ITemporaryTokenModel
+{
+  readonly token: string;
+  readonly type: TemporaryTokenTypeEnum;
+  readonly userIds: number[];
+  readonly expiredAt: Date;
+  readonly usedAt?: Date;
+
+  constructor({ id, createdAt, updatedAt, ...data }: ITemporaryTokenModel) {
+    super({ id, createdAt, updatedAt });
+    this.token = data.token;
+    this.type = data.type;
+    this.userIds = data.userIds;
+    this.expiredAt = data.expiredAt;
+    this.usedAt = data.usedAt;
+  }
+}
